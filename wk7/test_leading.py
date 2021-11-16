50fcf339d70c5fe34a7ef8daba498f5061f18c90
maltichon <69192963+maltichon@users.noreply.github.com>
2021-11-16 10:06:39 +0000

    minor changes

--- wk7/test_leading.py
+++ wk7/test_leading.py
@@ -5,35 +5,35 @@ import unittest
 import leading
 
 class TestLeading(unittest.TestCase):
-    '''Tests for leading'''
+    '''Tests for leading_substrings'''
 
     def test_empty(self):
         '''Test an empty string.'''
         inputted = ''
         leading.leading_substrings(inputted)
-        output_expected = ['']
-        self.assertEqual(output_expected, inputted, "The string is empty.")
+        output_expected = []
+        self.assertEqual([], leading.leading_substrings(inputted), "The string is empty.")
 
     def test_one(self):
         '''Test a one-character string'''
         inputted = 'a'
         leading.leading_substrings(inputted)
         output_expected = ['a']
-        self.assertEqual(output_expected, inputted, "The string contains one character.")
+        self.assertEqual(output_expected, leading.leading_substrings(inputted), "The string contains one character.")
 
     def test_two(self):
         '''Test a two-character string'''
         inputted = 'of'
         leading.leading_substrings(inputted)
         output_expected = ['o', 'of']
-        self.assertEqual(output_expected, inputted, "The string contains two characters.")
+        self.assertEqual(output_expected, leading.leading_substrings(inputted), "The string contains two characters.")
 
     def test_three(self):
         '''Test a three-character string'''
         inputted = 'cat'
         leading.leading_substrings(inputted)
         output_expected = ['c', 'ca', 'cat']
-        self.assertEqual(output_expected, inputted, "The string contains three characters.")
+        self.assertEqual(output_expected, leading.leading_substrings(inputted), "The string contains three characters.")
 
 
 if __name__ == '__main__':
